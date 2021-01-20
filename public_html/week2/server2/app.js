5373307007319b3a14b57cd160b96f0654535082
Sebastian Lara <selara@email.neit.edu>
2021-01-19 20:27:50 -0500

    alooo

--- public_html/week2/server2/app.js
+++ public_html/week2/server2/app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// partials directory setup
-const hbs = require('hbs');
+
+
 hbs.registerPartials(__dirname + '/views/partials');
 
 
--- public_html/week2/server2/routes/index.js
+++ public_html/week2/server2/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', nav: 'nav.hbs' });
+  res.render('index', { title: 'Express', nav: '_nav.hbs' });
   
 });
 
